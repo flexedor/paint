6495557ec4ee909da71050d2e73db4c73964904b
flexedor <fedor6574022@gmail.com>
2021-06-19 16:07:34 +0200

    first commit

---
+++ paint/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.horizontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(837, 474);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1033, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorToolStripMenuItem1,
+            this.horizontToolStripMenuItem});
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.colorToolStripMenuItem.Text = "tools";
+            // 
+            // colorToolStripMenuItem1
+            // 
+            this.colorToolStripMenuItem1.Name = "colorToolStripMenuItem1";
+            this.colorToolStripMenuItem1.Size = new System.Drawing.Size(126, 26);
+            this.colorToolStripMenuItem1.Text = "color";
+            this.colorToolStripMenuItem1.Click += new System.EventHandler(this.colorToolStripMenuItem1_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(52, 24);
+            this.saveToolStripMenuItem.Text = "save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.openToolStripMenuItem.Text = "open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click_1);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(901, 47);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(937, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // horizontToolStripMenuItem
+            // 
+            this.horizontToolStripMenuItem.Name = "horizontToolStripMenuItem";
+            this.horizontToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.horizontToolStripMenuItem.Text = "horizon";
+            this.horizontToolStripMenuItem.Click += new System.EventHandler(this.horizontToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1033, 572);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem horizontToolStripMenuItem;
+    }
+}
+
---
+++ paint/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            frec();
+        }
+      
+        Bitmap bmp;
+        Color color = Color.Black;
+        List<Point> lp = new List<Point>();
+        float roz = 4;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+           
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                lp.Add(new Point(e.X, e.Y));
+                fsend(e.X, e.Y);
+                Graphics g = Graphics.FromImage(bmp);
+                if (lp.Count()>1)
+                {
+                    g.DrawLines
+                       
+                        (new Pen(color, roz), lp.ToArray());
+                    
+                }
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            lp.Clear();
+        }
+
+        private void colorToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                color = colorDialog1.Color;
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            roz = Convert.ToSingle(numericUpDown1.Value);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            bmp = null;
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+        }
+        
+  
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                saveFileDialog1.Filter = "(bmp)|*.bmp|(jpeg)|*.jpg";
+                DialogResult dr = saveFileDialog1.ShowDialog();
+                if (dr == DialogResult.OK)
+                {
+                    bmp.Save(saveFileDialog1.FileName);
+                };
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void openToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.Filter = "(bmp)|*.bmp|(jpeg)|*.jpg|(png)|*.png";
+                DialogResult dr1 = openFileDialog1.ShowDialog();
+                if (dr1 == DialogResult.OK)
+                {
+                    bmp = new Bitmap(openFileDialog1.FileName);
+                    pictureBox1.Image = bmp;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+          
+        }
+
+        private void horizontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int sr;
+            int f = 0;
+            int k = 0;
+            for (int x = 0; x < bmp.Width; x++)
+                for (int y = 0; y < bmp.Height - 10; y++)
+                {
+                    //int sr=((bmp.GetPixel(x, y)).R+(bmp.GetPixel(x, y)).G+ (bmp.GetPixel(x, y)).B)/3;
+
+
+                    int r = (bmp.GetPixel(x, y)).R;
+                    int g = (bmp.GetPixel(x, y)).G;
+                    int b = (bmp.GetPixel(x, y)).B;
+                    sr = (r + g + b);
+
+                    int r1 = (bmp.GetPixel(x, y + 1)).R;
+                    int g1 = (bmp.GetPixel(x, y + 1)).G;
+                    int b1 = (bmp.GetPixel(x, y + 1)).B;
+                    int sr1 = (r1 + g1 + b1);
+                    if ((Math.Abs(r - r1) >= 1 || Math.Abs(b - b1) >= 1 || Math.Abs(b - b1) >= 1) && (r1 <= 165 && g1 <= 175 && b1 <= 165))
+                    {
+                        int[] ac = new int[10];
+                        int[] bc = new int[10];
+                        int[] cc = new int[10];
+                        for (int i = 0; i < 10; ++i)
+                        {
+                            ac[i] = (bmp.GetPixel(x, y + i)).R;
+                            bc[i] = (bmp.GetPixel(x, y + i)).G;
+                            cc[i] = (bmp.GetPixel(x, y + i)).B;
+                            if ((Math.Abs(r - ac[i]) >= 60 || Math.Abs(b - bc[i]) >= 5 || Math.Abs(b - cc[i]) >= 15) && (ac[i] <= 165 && bc[i] <= 165 && cc[i] <= 150) && bc[i] >= cc[i] && ac[i] <= 160)
+                            {
+                                k++;
+                            }
+                        }
+                        if (k >= 8)
+                        {
+                            if (lp.Count < 2) lp.Add(new Point(x, y));
+                            else
+                            {
+                                Point p2 = new Point(x + 1, y);
+                                Graphics gr = Graphics.FromImage(bmp);
+                                gr.DrawLine(new Pen(Color.Red, 3), lp.First(), p2);
+                                gr.DrawLine(new Pen(Color.Red, 3), lp.First(), lp.Last());
+
+                                lp.Clear();
+                                lp.Add(new Point(x, y));
+                            }
+                            f++;
+                        }
+                    }
+                    if (f == 1)
+                    {
+                        k = 0; f = 0; break;
+                    }
+
+
+                }
+            pictureBox1.Image = bmp;
+            lp.Clear();
+        }
+        void fsend(int x, int y)
+        {
+
+            byte[] bytCommand = new byte[] { };
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect(IPAddress.Parse("10.0.0.212"), 8000);
+            bytCommand = Encoding.ASCII.GetBytes("#(" + x + "," + y + ")");
+            udpClient.Send(bytCommand, bytCommand.Length);
+            pictureBox1.CreateGraphics().FillEllipse(Brushes.Black, x - 2, y - 2, 4, 4);
+
+
+        }
+
+
+        void frec()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            byte[] bytCommand = new byte[] { };
+            var receivingUdpClient = new System.Net.Sockets.UdpClient(8003);
+
+            Thread th = new Thread(() => {
+                IPEndPoint iep = new System.Net.IPEndPoint(System.Net.IPAddress.Any, 0);
+                while (true)
+                {
+                    byte[] receiveBytes = receivingUdpClient.Receive(ref iep);
+
+                    string str = System.Text.Encoding.ASCII.GetString(receiveBytes);
+
+                    int x = Convert.ToInt32(str.Replace("#", "").Replace("(", "").Replace(")", "").Split(',')[0]);
+                    int y = Convert.ToInt32(str.Replace("#", "").Replace("(", "").Replace(")", "").Split(',')[1]);
+
+                    pictureBox1.CreateGraphics().DrawEllipse(new Pen(Brushes.Black, 4), x - 2, y - 2, 4, 4);
+
+                }
+
+            });
+            th.IsBackground = true;
+            th.Start();
+
+        }
+     
+    }
+}
